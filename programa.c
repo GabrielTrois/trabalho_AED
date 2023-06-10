20a63172e26fe085d5e39dec2f1deb06a2d7c5c7
Gabriel Augusto <decauser@hotmail.com>
2023-06-10 08:27:37 -0300

    Mudanças no menu

--- programa.c
+++ programa.c
@@ -71,117 +71,173 @@ int main() {
 
     Lista* time = criarLista();
 
+    int op, tipoEquipe = 1;
+
+    char nome[50];
+
+    int position;
+
     while(1){
-        int op;
-        printf("Gerencie seus Pokemons:\n\n");
-        printf("Ver Equipe - 1\n");
-        printf("Adicionar Pokemon - 2\n");
-        printf("Remover pokemon - 3\n");
-        printf("Limpar sua Equipe - 4\n");
-        printf("Checar mais forte - 5\n");
-        printf("Checar mais fraco - 6\n");
-        printf("Crie seu Pokemon - 7\n");
-        printf("&@#*&($&@ - 8\n");
-        printf("Va para sua Aventura! - 9\n\n");
+        if(tipoEquipe == 1){
+            printf("Gerencie seus Pokemons:\n\n");
+            printf("Mudar para Pokedex - 0\n");
+            printf("Ver Equipe - 1\n");
+            printf("Adicionar Pokemon - 2\n");
+            printf("Remover pokemon - 3\n");
+            printf("Limpar sua Equipe - 4\n");
+            printf("Checar mais forte - 5\n");
+            printf("Checar mais fraco - 6\n");
+            printf("Crie seu Pokemon - 7\n");
+            printf("&@#*&($&@ - 8\n");
+            printf("Va para sua Aventura! - 9\n\n");
 
-        scanf("%d", &op);
+            scanf("%d", &op);
 
-        printf("\n");
+            printf("\n");
 
-        char nome[50];
-
-        switch(op){
-            case 1:
-                if(time->qtd == 0){
-                    printf("Voce nao tem nenhum Pokemon\n");
+            switch(op){
+                case 0:
+                    tipoEquipe = 2;
                     break;
-                }
-                imprimeLista(time);
-                break;
-            case 2:
-                if(time->qtd >= 6){
-                    printf("Sua equipe esta cheia\n");
+                case 1:
+                    if(time->qtd == 0){
+                        printf("Voce nao tem nenhum Pokemon\n");
+                        break;
+                    }
+                    imprimeLista(time);
                     break;
-                }
-                printf("Digite o nome do Pokemon: ");
-                scanf("%s", nome);
-                adicionaNoOutraLista(lista, time, nome);
-                break;
-            case 3:
-                if(time->qtd == 0){
-                    printf("Voce nao tem nenhum Pokemon\n");
+                case 2:
+                    if(time->qtd >= 6){
+                        printf("Sua equipe esta cheia\n");
+                        break;
+                    }
+                    printf("Digite o nome do Pokemon: ");
+                    scanf("%s", nome);
+                    adicionaNoOutraLista(lista, time, nome);
                     break;
-                }
-                printf("Digite o nome do Pokemon: ");
-                scanf("%s", nome);
-                removeNo(time, getPositionByName(time, nome));
-                break;
-            case 4:
-                if(time->qtd == 0){
-                    printf("Voce nao tem nenhum Pokemon\n");
+                case 3:
+                    if(time->qtd == 0){
+                        printf("Voce nao tem nenhum Pokemon\n");
+                        break;
+                    }
+                    printf("Digite o nome do Pokemon: ");
+                    scanf("%s", nome);
+                    removeNo(time, getPositionByName(time, nome));
                     break;
-                }
-                for(i = 0; i < 6; i++){
-                   removeNo(time, 1);
-                }
-                printf("Equipe limpa\n");
-                break;
-            case 5:
-                if(time->qtd == 0){
-                    printf("Voce nao tem nenhum Pokemon\n");
-                    break;
-                }
-                printf("Seu Pokemon mais forte eh: \n");
-                imprimeMinMaxTotal(time, 1);
-                break;
-            case 6:
-                if(time->qtd == 0){
-                    printf("Voce nao tem nenhum Pokemon\n");
-                    break;
-                }
-                printf("Seu Pokemon mais fraco eh: \n");
-                imprimeMinMaxTotal(time, 2);
-                break;
-            case 7:
-                if(time->qtd >= 6){
-                    printf("Sua equipe esta cheia\n");
-                    break;
-                }
-                char tipo1[20], tipo2[20];
-                int total, hp, attack, defense, sp_atk, sp_def, speed, gen;
-
-                printf("Escreva o nome do seu Pokemon: ");
-                scanf("%s", nome);
-                printf("\nEscreva os tipos do seu Pokemon(Escreva none para nenhum tipo): ");
-                scanf("%s %s", tipo1, tipo2);
-                printf("\nEscreva a vida do seu Pokemon: ");
-                scanf("%d", &hp);
-                printf("\nEscreva os status de ataque, defesa, ataque especial, defesa especial e velocidade do seu Pokemon: ");
-                scanf("%d %d %d %d %d", &attack, &defense, &sp_atk, &sp_def, &speed);
-                printf("\nEscreva a geracao do seu pokemon: ");
-                scanf("%d", &gen);
-
-                total = attack + defense + sp_atk + sp_def + speed;
-
-                adicionarNoManual(time, 0, nome, tipo1, tipo2, total, hp, attack, defense, sp_atk, sp_def, speed, gen, "TRUE");
-
-                printf("Seu Pokemon foi criado\n");
-
-                break;
-            case 8:
-                if(time->qtd >= 6){
+                case 4:
+                    if(time->qtd == 0){
+                        printf("Voce nao tem nenhum Pokemon\n");
+                        break;
+                    }
+                    for(i = 0; i < 6; i++){
                     removeNo(time, 1);
-                }
-                adicionarNoManual(time, 152, "Missingno", "Bird", "Normal", 204, 33, 136, 0, 6, 6, 29, 1, "TRUE");
-                printf("PoK3m0n Ad1CioNado cOm susucessOO!!@!?\n");
-                break;
-            case 9:
-                printf("\nVoce sai em sua aventura!\n");
-                return 0;
-            }
-        system("pause");
-        system("cls");
-    }
+                    }
+                    printf("Equipe limpa\n");
+                    break;
+                case 5:
+                    if(time->qtd == 0){
+                        printf("Voce nao tem nenhum Pokemon\n");
+                        break;
+                    }
+                    printf("Seu Pokemon mais forte eh: \n");
+                    imprimeMinMaxTotal(time, 1);
+                    break;
+                case 6:
+                    if(time->qtd == 0){
+                        printf("Voce nao tem nenhum Pokemon\n");
+                        break;
+                    }
+                    printf("Seu Pokemon mais fraco eh: \n");
+                    imprimeMinMaxTotal(time, 2);
+                    break;
+                case 7:
+                    if(time->qtd >= 6){
+                        printf("Sua equipe esta cheia\n");
+                        break;
+                    }
+                    char tipo1[20], tipo2[20];
+                    int total, hp, attack, defense, sp_atk, sp_def, speed, gen;
 
+                    printf("Escreva o nome do seu Pokemon: ");
+                    scanf("%s", nome);
+                    printf("\nEscreva os tipos do seu Pokemon(Escreva none para nenhum tipo): ");
+                    scanf("%s %s", tipo1, tipo2);
+                    printf("\nEscreva a vida do seu Pokemon: ");
+                    scanf("%d", &hp);
+                    printf("\nEscreva os status de ataque, defesa, ataque especial, defesa especial e velocidade do seu Pokemon: ");
+                    scanf("%d %d %d %d %d", &attack, &defense, &sp_atk, &sp_def, &speed);
+                    printf("\nEscreva a geracao do seu pokemon: ");
+                    scanf("%d", &gen);
+
+                    total = attack + defense + sp_atk + sp_def + speed;
+
+                    adicionarNoManual(time, 0, nome, tipo1, tipo2, total, hp, attack, defense, sp_atk, sp_def, speed, gen, "TRUE");
+
+                    printf("Seu Pokemon foi criado\n");
+
+                    break;
+                case 8:
+                    if(time->qtd >= 6){
+                        removeNo(time, 1);
+                    }
+                    adicionarNoManual(time, 152, "Missingno", "Bird", "Normal", 204, 33, 136, 0, 6, 6, 29, 1, "TRUE");
+                    printf("PoK3m0n Ad1CioNado cOm susucessOO!!@!?\n");
+                    break;
+                case 9:
+                    printf("\nVoce sai em sua aventura!\n");
+                    return 0;
+                default:
+                    printf("Digite uma operacao valida");
+                    break;
+            }
+            system("pause");
+            system("cls");
+        }
+        else{
+            printf("Gerencie sua Pokedex:\n\n");
+            printf("Mudar para Equipe - 0\n");
+            printf("Ver Pokedex - 1\n");
+            printf("Exibir mais forte - 2\n");
+            printf("Exibir mais fraco - 3\n");
+            printf("Exibir Pokemon por nome - 4\n");
+            printf("Exibir Pokemon por posicao - 5\n\n");
+
+            scanf("%d", &op);
+
+            printf("\n");
+
+            switch(op){
+                case 0:
+                    tipoEquipe = 1;
+                    break;
+                case 1:
+                    imprimeLista(lista);
+                    break;
+                case 2:
+                    printf("Seu Pokemon mais forte eh: \n");
+                    imprimeMinMaxTotal(lista, 1);
+                    break;
+                case 3:
+                    printf("Seu Pokemon mais fraco eh: \n");
+                    imprimeMinMaxTotal(lista, 2);
+                    break;
+                case 4:
+                    printf("Digite o nome do Pokemon: ");
+                    scanf("%s", nome);
+                    imprimeNoByPosition(lista, getPositionByName(lista, nome)-1);
+                    break;
+                case 5:
+                    printf("Digite a posicao do Pokemon: ");
+                    scanf("%d", &position);
+                    imprimeNoByPosition(lista, position-1);
+                    break;
+                default:
+                    printf("Digite uma operacao valida");
+                    break;
+            }
+            system("pause");
+            system("cls");
+        }
+    }
     return 0;
 }
